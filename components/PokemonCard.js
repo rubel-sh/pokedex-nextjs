45784c4423d97ec7594e83e9fb25c1ca4059fbd4
Rubel Hossain <rubrex96@hotmail.com>
2023-01-27 06:20:50 +0600

    installed tailwincss

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,5 +1,10 @@
 const PokemonCard = ({ demo }) => {
-  return <div>Catch the pokemon{demo}</div>;
+  return (
+    <div>
+      Catch the pokemon{demo}
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    </div>
+  );
 };
 
 export default PokemonCard;
---
+++ pages/pokemon/index.js
@@ -0,0 +1,5 @@
+const index = () => {
+  return <div>Pokemon</div>;
+};
+
+export default index;
